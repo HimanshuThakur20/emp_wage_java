8183f2a6822efeb8325c736a4c1936ac9fcd3e73
Himanshu <himamshuthakur20@gmail.com>
2024-01-22 03:37:33 +0530

    [Himanshu]: Feature added: Attendence

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("\nWelcome to Employee Wage Computation Program\n");
+
+        Attendance A1 = new Attendance();
+        Employee emp1 = new Employee("Himanshu",123, A1);
+        System.out.println(emp1);
+
+
+
+//        Employee emp2 = new Employee("Himanshu",122);
+//        System.out.println(emp2);
+
+
+
     }
 }
